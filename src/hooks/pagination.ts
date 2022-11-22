06d1fbe1f7f47797a75eed25f0490ba243b8664b
mewcoder <1010953107@qq.com>
2022-11-22 19:58:23 +0800

    feat: mod table

---
+++ src/hooks/pagination.ts
@@ -0,0 +1,25 @@
+import { reactive } from 'vue';
+
+export function usePagination(getList: any) {
+  const pagination = reactive({
+    pageNo: 1, // 当前页码
+    pageSize: 20, // 每页显示数量
+    total: 0 // 总数
+  });
+  
+  const handleSizeChange = (pageSize: number) => {
+    pagination.pageSize = pageSize;
+    getList();
+  };
+
+  const handleCurrentChange = (currentPage: number) => {
+    pagination.pageNo = currentPage;
+    getList();
+  };
+
+  return {
+    pagination,
+    handleSizeChange,
+    handleCurrentChange
+  };
+}
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,12 +1,68 @@
 <template>
+  <el-form :model="form" label-position="top" label-width="120px">
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-form-item label="Name">
+          <el-input v-model="form.name" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="6">
+        <el-form-item label="Region">
+          <el-input v-model="form.region" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="6">
+        <el-form-item label="Resource">
+          <el-input v-model="form.resource" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="6">
+        <el-form-item label="Description">
+          <el-input v-model="form.description" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+    <div style="float: right">
+      <el-button type="primary" @click="onSubmit">Search</el-button>
+      <el-button>Clear</el-button>
+    </div>
+  </el-form>
   <el-table :data="tableData" style="width: 100%">
     <el-table-column prop="date" label="Date" width="180" />
     <el-table-column prop="name" label="Name" width="180" />
     <el-table-column prop="address" label="Address" />
   </el-table>
+  <el-pagination
+    v-model:current-page="pagination.pageNo"
+    v-model:page-size="pagination.pageSize"
+    :page-sizes="[10, 20, 50, 100]"
+    layout="total, sizes, prev, pager, next"
+    :total="pagination.total"
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+  />
 </template>
 
 <script lang="ts" setup>
+import { usePagination } from '@/hooks/pagination';
+import { reactive } from 'vue';
+
+// do not use same name with ref
+const form = reactive({
+  name: '',
+  region: '',
+  resource: '',
+  description: ''
+});
+
+const onSubmit = () => {
+  console.log('submit!');
+};
+
+const { pagination, handleCurrentChange, handleSizeChange } = usePagination(
+  () => {}
+);
+
 const tableData = [
   {
     date: '2016-05-03',
