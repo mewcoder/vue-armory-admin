beb64d719d117e4c29a889dc54b2e352e54043db
mewcoder <mewcoder@users.noreply.github.com>
2022-11-23 12:36:26 +0000

    deploy: 5fae1550f5d420a97ce47b9a508c5de98659610c

--- assets/index.09be33e1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t}from"./index.32f29641.js";const n={name:"AboutView"};function a(c,r,s,_,p,i){return o(),t("main",null," about ")}const f=e(n,[["render",a]]);export{f as default};
